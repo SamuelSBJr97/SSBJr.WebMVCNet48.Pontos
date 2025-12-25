428be706bc251aad4e37a1e332c6852157773760
Samuel S B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-12-25 08:50:21 -0300

    update

--- SSBJr.WebMVCNet48.Pontos/App_Start/BundleConfig.cs
+++ SSBJr.WebMVCNet48.Pontos/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace Pontos
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
+                        "~/Scripts/popper.min.js"));
+
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
